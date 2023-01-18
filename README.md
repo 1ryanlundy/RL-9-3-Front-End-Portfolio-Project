a25a0c99dac1e721d98135e96da6b75274990593
Ryan Lundy <rlundy1792@gmail.com>
2023-01-18 02:59:28 -0500

    Updeated functionality and added comments

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Random Movie / Movie Search App
-
+* When visiting the site a random movie that is the first in a trilogy or a series of movies will load on on screen. 
+* You can search for a movie of your choice by typing the title in the text box on the bottom of the site and then pressing the "Search Movie" button 
+* Reload the page to load another random movie that is the first in a trilogy or a series of movies will load on on screen.
--- main.js
+++ main.js
@@ -42,15 +42,16 @@ const API_KEY = "3dfb80d7";
 let load = true;
 // Creating a variable to store the value of the URL link being fetched upon loading the site 
 let onLoadURL = `http://www.omdbapi.com/?apikey=${API_KEY}&t=${randomMovie}`;
-//   
+// Load variable is true and makeFetch() will run with onLoadURL as its argument   
 load && makeFetch(onLoadURL);
-// 
+// Creating variable and initiating its value to the DOM element with .random_result class
 const randomResultDiv = document.querySelector(".random_result");
+// 
+const titleH1 = document.querySelector("#title_h1");
+
 // Creating function to load information about a random movie when loading the site
 function loadRandomMovie(fetchData) {
-  //Changing load variable to false now that the initial load was done
-  load = false;
-  // 
+  // Using .innerHTML to create HTML elements that will display information about a random movie 
   randomResultDiv.innerHTML = `
   <h3 class="text-center mt-2" class="movie_title">${fetchData.Title}</h3>
   <img class="rounded mx-auto d-block mt-4" class="img-thumbnail" width="400" height"400" src="${fetchData.Poster}" alt="Movie poster"/>    
@@ -60,41 +61,49 @@ function loadRandomMovie(fetchData) {
   <p class="text-center">Directed by: ${fetchData.Director}</p>
   <p class="text-center">Genre: ${fetchData.Genre}</p>
   `
-  
+  //Changing load variable to false now that the initial load was done
+  load = false;
 }
 
-// 
+// Creating variable and initiating its value to the  DOM element that has an id of #movie_input_form
 const movieInputForm = document.querySelector("#movie_input_form");
-// 
+// Adding an event listener to movieInputForm that listens for a submit event
 movieInputForm.addEventListener("submit", movieInputSubmit => {
     movieInputSubmit.preventDefault();
-    // 
-    var movieInput = document.querySelector("#movie_input").value
-    // 
+    // Creating variable and initiating its value to the DOM element with #movie_input ID
+    const movieInput = document.querySelector("#movie_input").value
+    // Creating variable equals URL link  for makeFetch() function
     const movieSearchURL = `http://www.omdbapi.com/?apikey=${API_KEY}&t=${movieInput}`  
-    // 
+    // Calling makeFetch() function with movieSearchURL variable as argument 
     makeFetch(movieSearchURL)
-    // 
+    // Using .value to set DOM element with #movie_input ID to an empty string
     movie_input.value = "";
+    // Using.textContent to change H1 text to "Search Results"
+    titleH1.textContent = "Search Result:"
 });
 
+// Creating variable to get user search results
 function getUserSearch(fetchData) {
+  // Using .remove to remove randomResultDiv variable 
   randomResultDiv.remove();
+  // Creating variable and initiating its value to the DOM element with .movie_input class
   const searchResultDiv = document.querySelector(".search_result");
+  // Using if statement to determine if the value of the .title of fetchData object is undefined 
   if (fetchData.Title === undefined) {
+    // Using .innerHTML to add H5 element with text message that asks user to enter a valid movie title
     searchResultDiv.innerHTML = `
     <h5 class="text-center mt-4"> Please enter a valid movie title.</h5>
     `
-
   } else {
-  searchResultDiv.innerHTML = `
-  <h3 class="text-center mt-2" class="movie_title">${fetchData.Title}</h3>
-  <img class="rounded mx-auto d-block mt-4" class="img-thumbnail" width="400" height"400" src="${fetchData.Poster}"/> 
-  <p class="text-center mt-2">${fetchData.Year}</p>
-  <p class="text-center mt-3">${fetchData.Plot}</p>
-  <p class="text-center">Rated: ${fetchData.Rated}</p>
-  <p class="text-center">Directed by: ${fetchData.Director}</p>
-  <p class="text-center">Genre: ${fetchData.Genre}</p>
+    // Using .innerHTML to add HTML elements that display information about searched movie
+    searchResultDiv.innerHTML = `
+    <h3 class="text-center mt-2" class="movie_title">${fetchData.Title}</h3>
+    <img class="rounded mx-auto d-block mt-4" class="img-thumbnail" width="400" height"400" src="${fetchData.Poster}"/> 
+    <p class="text-center mt-2">${fetchData.Year}</p>
+    <p class="text-center mt-3">${fetchData.Plot}</p>
+    <p class="text-center">Rated: ${fetchData.Rated}</p>
+    <p class="text-center">Directed by: ${fetchData.Director}</p>
+    <p class="text-center">Genre: ${fetchData.Genre}</p>
   `
   }
 };
